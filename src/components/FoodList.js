0741dc0da317e95d60d2534c8e445527efe7fc03
JiinSeok <seokjiin1073@gmail.com>
2024-05-22 11:43:12 +0900

    feat: Foodit 아이템 렌더링

---
+++ src/components/FoodList.js
@@ -0,0 +1,27 @@
+function FoodListItem({ item }) {
+  const { imgUrl, title, calorie, content } = item;
+
+  return (
+    <div>
+      <img src={imgUrl} alt={title} />
+      <div>{title}</div>
+      <div>{calorie}</div>
+      <div>{content}</div>
+    </div>
+  );
+}
+
+function FoodList({ items }) {
+  return (
+    <ul>
+      {items.map((item) => (
+        // items라는 Prop의 요소마다 <li> 태그를 렌더링 // <li> 태그 안에는 FoodListItem을 렌더링
+        <li key={item.id}>
+          <FoodListItem item={item} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default FoodList;
