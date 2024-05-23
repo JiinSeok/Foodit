2ad793d46e66ddd41e71165e9ec62afd8e2909f8
JiinSeok <seokjiin1073@gmail.com>
2024-05-23 15:19:07 +0900

    feat: 더 보기 버튼 추가

--- src/api.js
+++ src/api.js
@@ -1,5 +1,16 @@
-export async function getFoods() {
-  const response = await fetch("https://learn.codeit.kr/0220/foods");
+export async function getFoods({ order = "", cursor = "", limit = 10 }) {
+  let query = "";
+  if (order) {
+    query += `order=${order}`;
+  }
+  if (cursor) {
+    query += `&cursor=${cursor}`;
+  }
+  if (limit > 0) {
+    query += `&limit=${limit}`;
+  }
+  // const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
+  const response = await fetch(`https://learn.codeit.kr/0220/foods/?${query}`);
   const body = response.json();
   return body;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,37 @@ import { getFoods } from "../api"; // 함수 import
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null);
 
   const handleNewestClick = () => setOrder("createdAt");
+
   const handleBestClick = () => setOrder("calorie");
+
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
-  const handleLoad = async () => {
-    const { foods } = await getFoods();
-    setItems(foods);
+
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, cursor });
   };
 
   useEffect(() => {
-    handleLoad();
-  }, []);
+    handleLoad({ order });
+  }, [order]);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -28,6 +44,7 @@ function App() {
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleBestClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더 보기</button>}
     </div>
   );
 }
