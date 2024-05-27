a7838e03eb25f4ce0a63e93a146ea53de186011e
JiinSeok <seokjiin1073@gmail.com>
2024-05-27 22:01:51 +0900

    feat: 파일 첨부 기능 추가

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import FileInput from "./FileInput";
 
 function FoodForm() {
   const [values, setValues] = useState({
+    imgFile: null,
     title: "",
     calorie: 0,
     content: "",
@@ -17,13 +19,17 @@ function FoodForm() {
     }
   };
 
-  const handleChange = (e) => {
-    const { name, value, type } = e.target; // e.target 값을 name 과 value 로 Destructuring
+  const handleChange = (name, value) => {
     setValues((preValues) => ({
       // 이 값을 활용해서 values 스테이트를 변경
       ...preValues,
-      [name]: sanitize(type, value),
+      [name]: value,
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value, type } = e.target; // e.target 값을 name 과 value 로 Destructuring
+    handleChange(name, sanitize(value, type));
   };
 
   const handleSubmit = (e) => {
@@ -33,17 +39,26 @@ function FoodForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input name='title' value={values.title} onChange={handleChange}></input>
+      <FileInput
+        name='imgFile'
+        value={values.imgFile}
+        onChange={handleChange}
+      />
+      <input
+        name='title'
+        value={values.title}
+        onChange={handleInputChange}
+      ></input>
       <input
         type='number'
         name='calorie'
         value={values.calorie}
-        onChange={handleChange}
+        onChange={handleInputChange}
       ></input>
       <input
         name='content'
         value={values.content}
-        onChange={handleChange}
+        onChange={handleInputChange}
       ></input>
       <button type='submit'>확인</button>
     </form>
