2dd7923f415178192973be7c15a2e883f73f0a54
jiin.seok <seokjiin1073@gmail.com>
2024-05-28 17:14:35 +0900

    refactor

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 
 function FileInput({ name, value, onChange }) {
-  const inputRef = useRef(); // DOM에 직접 접근
+  const inputRef = useRef(); // DOM에 직접 접근하는 Ref 객체 생성
 
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
